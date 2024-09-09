c329df9749096413161ce238ed063080f65a3c4c
Islam Ibrakhimzhanov <36242677+Ibrakhimzhanov@users.noreply.github.com>
2024-09-09 16:31:43 +0500

    Типизация модуля timeline items и исправления некоторых ошибок

--- src/components/NavItem.vue
+++ src/components/NavItem.vue
@@ -4,6 +4,8 @@ import { PAGE_TIMELINE } from '../constants'
 import { currentPage, navigate } from '../router'
 import { scrollToCurrentHour } from '../timeline-items'
 import { isNavItemValid } from '../validators'
+import BaseIcon from './BaseIcon.vue'
+
 
 const props = defineProps({
   navItem: {
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import { startCurrentDateTimer } from './time'
 import { syncState } from './storage'
+
 import App from './App.vue'
 
 import './assets/main.css'
